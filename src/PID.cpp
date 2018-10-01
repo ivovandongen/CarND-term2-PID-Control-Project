4f80c952dc6ab1b8969258433ab1f4ab67274c16
Ivo van Dongen <info@ivovandongen.nl>
2018-10-01 16:48:33 +0300

    PID implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,31 @@
 #include "PID.hpp"
 
-/*
-* TODO: Complete the PID class.
-*/
+#include <iostream>
 
 PID::PID() = default;
 
 PID::~PID() = default;
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::init(double Kp_, double Ki_, double Kd_) {
+    // Set the params
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
+
+    // Initialize the errors
+    p_error = i_error = d_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::updateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
-double PID::TotalError() {
-    // TODO
-    return 0;
+double PID::totalError() {
+//    std::cout << "P: " << -Kp * p_error << std::endl;
+//    std::cout << "D: " << -Kd * d_error << std::endl;
+//    std::cout << "I: " << -Ki * i_error << std::endl;
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
--- src/PID.hpp
+++ src/PID.hpp
@@ -1,46 +1,44 @@
-#ifndef PID_H
-#define PID_H
+#pragma once
 
 class PID {
 public:
-    /*
-    * Errors
-    */
+    /**
+     * Constructor
+     */
+    PID();
+
+    /**
+     * Destructor.
+     */
+    virtual ~PID();
+
+    /**
+     * Initialize PID.
+     */
+    void init(double Kp, double Ki, double Kd);
+
+    /**
+     * Update the PID error variables given cross track error.
+     */
+    void updateError(double cte);
+
+    /**
+     * Calculate the total PID error.
+     */
+    double totalError();
+
+private:
+    /**
+     * Errors
+     */
     double p_error;
     double i_error;
     double d_error;
 
-    /*
-    * Coefficients
-    */
+    /**
+     * Coefficients
+     */
     double Kp;
     double Ki;
     double Kd;
-
-    /*
-    * Constructor
-    */
-    PID();
-
-    /*
-    * Destructor.
-    */
-    virtual ~PID();
-
-    /*
-    * Initialize PID.
-    */
-    void Init(double Kp, double Ki, double Kd);
-
-    /*
-    * Update the PID error variables given cross track error.
-    */
-    void UpdateError(double cte);
-
-    /*
-    * Calculate the total PID error.
-    */
-    double TotalError();
 };
-
-#endif /* PID_H */
