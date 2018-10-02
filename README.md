ade0043d12bceea67b8030a3e54524940337d05e
Ivo van Dongen <info@ivovandongen.nl>
2018-10-02 12:11:19 +0300

    optionally run twiddle

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Part of the Self-Driving Car nanodegree. Starter code and data from [Udacity](ht
 ## Build
 - `#>` `git submodule update --init --recursive`
 - `#>` `mkdir build && cd build && cmake .. -DGIT_SUBMODULE=ON && make`
+
+# Run
+
 - `#>` `./pid`
+
+# Run with Twiddle optimisation
+
+- `#>` `./pid twiddle`
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <uWS.h>
 #include <json.hpp>
 #include <PID.hpp>
+#include <twiddle.hpp>
 
 
 #include <iostream>
+#include <limits>
 #include <math.h>
 
 // for convenience
@@ -32,30 +34,39 @@ std::string hasData(std::string s) {
 }
 
 void reset(uWS::WebSocket<uWS::SERVER> &ws, PID &pid) {
-    std::cout << "=========== RESET ==========" << std::endl;
+    std::cout << "Reset simulator" << std::endl;
     std::string msg = "42[\"reset\",{}]";
     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-
-    // TODO: Optimize the pid variable.
-    // Values from course: 0.2, 0.004, 3.0
-    pid.init(0.1, 0.001, 3.0);
 }
 
-int main() {
+int run(const std::array<double, 3> &p, double &errorOut, int maxRuns = -1) {
+    std::cout << "==== Start run with params: " << p[0] << " " << p[1] << " " << p[2] << std::endl;
     uWS::Hub h;
+
     PID pid;
+
     bool fresh = false;
 
-    h.onMessage([&pid, &fresh](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    long runNum = 0;
+    long thresh = maxRuns / 2;
+
+    h.onMessage([&](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                    uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
-        if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+        if (length > 2 && data[0] == '4' && data[1] == '2') {
 
             // Start fresh
             if (!fresh) {
+                // Reset sim
                 reset(ws, pid);
+
+                // Reset PID
+                pid.init(p[0], p[1], p[2]);
+
                 fresh = true;
+                return;
             }
 
             auto s = hasData(std::string(data).substr(0, length));
@@ -63,34 +74,52 @@ int main() {
                 auto j = json::parse(s);
                 std::string event = j[0].get<std::string>();
                 if (event == "telemetry") {
+                    runNum++;
+
                     // j[1] is the data JSON object
                     double cte = std::stod(j[1]["cte"].get<std::string>());
                     double speed = std::stod(j[1]["speed"].get<std::string>());
                     double angle = std::stod(j[1]["steering_angle"].get<std::string>());
 
-                    /*
-                    * TODO: Calculate steering value here, remember the steering value is
-                    * [-1, 1].
-                    * NOTE: Feel free to play around with the throttle and speed. Maybe use
-                    * another PID controller to control the speed!
-                    */
-
                     // Steering
                     pid.updateError(cte);
                     double steer_value = pid.totalError();
 
-                    // Speed
-                    double throttle = std::max<double>(speed < 0 ? 0 : -1, 1 - speed / 15 * std::abs(steer_value));
+                    // Speed:
+                    // - only start actively break over 20 mph to avoid going backwards
+                    // - reduce throttle with greater stearing angles over 20 mph to avoid
+                    //   losing control
+                    double throttle = std::max<double>(speed > 20 ? -1 : 0,
+                                                       speed > 20 ? 1 - speed / 15 * std::abs(steer_value) : 1);
 
                     // DEBUG
-                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+                    if (maxRuns < 0) {
+                        std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+                    }
 
                     json msgJson;
                     msgJson["steering_angle"] = steer_value;
                     msgJson["throttle"] = throttle;
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-                    std::cout << msg << std::endl;
+
+                    // DEBUG
+                    if (maxRuns < 0) {
+                        std::cout << msg << std::endl;
+                    }
+
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
+                    // Report error if twiddling
+                    if (maxRuns > 0 && runNum > thresh) {
+                        errorOut += cte * cte;
+                    }
+
+                    // End if max runs set and exceeded
+                    if (maxRuns > 0 && runNum >= maxRuns) {
+                        //End run
+                        std::cout << "End of run" << std::endl;
+                        h.getDefaultGroup<uWS::SERVER>().close();
+                    }
                 }
             } else {
                 // Manual driving
@@ -104,21 +133,7 @@ int main() {
     // doesn't compile :-(
     h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
         const std::string s = "<h1>Hello world!</h1>";
-        if (req.getUrl().valueLength == 1) {
-            res->end(s.data(), s.length());
-        } else {
-            // i guess this should be done more gracefully?
-            res->end(nullptr, 0);
-        }
-    });
-
-    h.onConnection([&h, &fresh](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-        std::cout << "Connected!!!" << std::endl;
-    });
-
-    h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-        ws.close();
-        std::cout << "Disconnected" << std::endl;
+        res->end(s.data(), s.length());
     });
 
     int port = 4567;
@@ -129,4 +144,45 @@ int main() {
         return -1;
     }
     h.run();
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    const bool runTwiddle = argc > 1;
+
+    double totalError = 0;
+
+    if (runTwiddle) {
+        std::cout << "Running twiddle" << std::endl << std::endl;
+
+        // Tweakable; number of steps per run
+        int stepsPerRun = 1000;
+
+        auto bestParams = twiddle<3>({.2, .004, 3}, // Initialize with values from course
+                                     [&](std::array<double, 3> p) {
+                                         totalError = 0;
+                                         if (run(p, totalError, stepsPerRun) < 0) {
+                                             //Failed
+                                             std::cerr << "Run failed" << std::endl;
+                                             exit(1);
+                                             return std::numeric_limits<double>::max();
+                                         } else {
+                                             std::cout << "Total error: " << totalError << std::endl;
+                                             return totalError;
+                                         }
+                                     },
+                                     0.2 // Tweakable; tolerance
+        );
+
+        std::cout << std::endl << "=========" << std::endl;
+        std::cout << "Best params: " << bestParams[0] << " " << bestParams[1] << " " << bestParams[2] << std::endl;
+    } else {
+        // Manually tuned parameters
+        run({.1, .001, 3}, totalError);
+
+        // Twiddle optimized
+//        run({0.45988, 0.00323188, 14.2266}, totalError);
+    }
+    std::cout << std::endl << "=========" << std::endl;
+    std::cout << std::endl << "Total error: " << totalError << std::endl;
 }
